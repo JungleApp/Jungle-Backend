8976b77e20d0a923cafe7a1afe9bfdb0217a2e8d
Jesse Bartola <jrbartola@gmail.com>
2016-11-26 16:55:09 -0500

    Added error logger

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ from flask import Flask, request, session, g, url_for, render_template
 
 app = Flask(__name__)
 
+
+# Setup our error logging
+if app.debug is not True:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('errors.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+    file_handler.setLevel(logging.ERROR)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
+
 @app.route('/')
 def index():
     return '''
