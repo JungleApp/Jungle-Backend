37c182a8279998c2a073ff2396a5c2451403dbdf
Jesse Bartola <jrbartola@gmail.com>
2017-04-17 23:51:14 -0400

    Auth.js LocalStrategy implementation

---
+++ server/utility/auth.js
@@ -0,0 +1,39 @@
+/**
+ * Passport.js Authentication
+ * Created 4/17/17 @jessebartola
+ */
+
+var passport = require('passport');
+LocalStrategy = require('passport-local').Strategy;
+var databaseCall = require('databaseCalls');
+var ngoSchema = mongoose.model('../routes/organization');
+var messages = require('messages');
+
+// Define new username/password login strategy
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+    	databaseCall.findOneQuery(ngoSchema, {'username': username}).then(function (response){
+        	var currentUser = response.data;
+
+        	// Db is not available
+        	if (response.isError) {
+
+        		return done(null, false, { message: 'Database not available'});
+
+        	} else if (response.errorMessage = messages.NOT_FOUND) {
+
+        		return done(null, false, { message: 'Invalid username.'});
+
+        	} else if (!currentUser.validPassword(password)) {
+
+        		return done(null, false, { message: 'Invalid password.'});
+
+        	} else {
+        		// If we have a valid username and correct password, confirm authentication
+        		return done(null, currentUser);
+        	}
+        	
+    	});
+    }
+));
+
